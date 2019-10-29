7bbd3ca707d72901320522c79ba242ded4561b07
Andrea Mogren <mogrenandrea@gmail.com>
2019-10-29 19:10:19 +0100

    continuing with optional exercises in part 3a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let notes = [
     {    
@@ -21,9 +22,12 @@ let notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {  
-    response.writeHead(200, { 'Content-Type': 'application/json' })  
-    response.end(JSON.stringify(notes))
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>')
+})
+
+app.get('/notes', (req, res) => {
+    res.json(notes)
 })
 
 const port = 3001
