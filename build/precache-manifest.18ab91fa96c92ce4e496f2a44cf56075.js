c2b1747158b8473ca2a0f8695ae5a7237a5073a2
Andrea <andrea.mogren@comprend.com>
2019-11-06 19:31:13 +0100

    adding front end

---
+++ build/precache-manifest.18ab91fa96c92ce4e496f2a44cf56075.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9998325f77600f01e716aa942daf140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3d6fd6ef928bb025b3",
+    "url": "/static/js/2.1b07f559.chunk.js"
+  },
+  {
+    "revision": "5e22e932caf8ce0cbb8b",
+    "url": "/static/js/main.cf47bf78.chunk.js"
+  },
+  {
+    "revision": "85f12bdc16839a22b169",
+    "url": "/static/js/runtime-main.22d3bac8.js"
+  }
+]);
