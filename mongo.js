17e6f2d28ebeb5aa4ac0c44da718389c6ec83516
Andrea <andrea.mogren@comprend.com>
2019-11-16 19:41:41 +0100

    exercise 3.22 done

--- mongo.js
+++ mongo.js
@@ -7,21 +7,21 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = 
+const url =
   `mongodb+srv://fullstack-course:${password}@cluster0-ws80k.mongodb.net/phonebook?retryWrites=true&w=majority`
 
-mongoose.connect(url, {useNewUrlParser: true})
+mongoose.connect(url, { useNewUrlParser: true })
 
 const personSchema = new mongoose.Schema({
-  name: String, 
-  number: String, 
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   Person.find({}).then(people => {
-    console.log("Phonebook:")
+    console.log('Phonebook:')
     people.forEach(person => {
       console.log(person.name, person.number)
       mongoose.connection.close()
@@ -34,7 +34,7 @@ if (process.argv.length >= 4) {
     name: process.argv[3],
     number: process.argv[4],
   })
-  
+
   person.save().then(people => {
     console.log(`Added ${person.name} ${person.number} to the phonebook`)
     mongoose.connection.close()
